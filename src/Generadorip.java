8d9c5d7ff648ed9e5b170eee4fa1771268fd76fb
Raúl <raulgr74@educastur.es>
2022-03-25 13:11:48 +0100

    Métodos implementados

--- src/Generadorip.java
+++ src/Generadorip.java
@@ -1,4 +1,17 @@
 
 public class Generadorip {
 
+	public static int generarNumero(int min, int max) {
+		return (int) (Math.random() * (max - min) + min);
+	}
+
+	public static String generarIp() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i <= 3; i++) {
+			sb.append(generarNumero(0, 254));
+			sb.append(".");
+		}
+		sb.append(generarNumero(0, 254));
+		return sb.toString();
+	}
 }
